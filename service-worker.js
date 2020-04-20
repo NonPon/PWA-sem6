490536fa0b10b8beabfaa208bed728c4a2a879dc
Manon <NonPon97@gmail.com>
2020-04-20 15:52:49 +0200

    Bot geupload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+self.addEventListener("install",(installing)=>{
+    console.log("Service Worker: I am being installed, hello world!");
+  });
+  
+  self.addEventListener("activate",(activating)=>{	
+    console.log("Service Worker: All systems online, ready to go!");
+  });
+  
+  self.addEventListener("fetch",(fetching)=>{   
+    console.log("Service Worker: User threw a ball, I need to fetch it!");
+  });
+  
+  self.addEventListener("push",(pushing)=>{
+      console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
+      if(pushing.data){
+        pushdata=JSON.parse(pushing.data.text());		
+        console.log("Service Worker: I received this:",pushdata);
+        if((pushdata["title"]!="")&&(pushdata["message"]!="")){			
+          const options={ body:pushdata["message"], image:"https://unbateaurouge.files.wordpress.com/2014/04/belledebij-hi-res.jpg" }
+          self.registration.showNotification(pushdata["title"],options);
+          console.log("Service Worker: I made a notification for the user");
+        } else {
+          console.log("Service Worker: I didn't make a notification for the user, not all the info was there :(");			
+        }
+      }
+      
+  })
+
+  self.addEventListener("notificationclick",function(clicking){
+    const pageToOpen="/";
+    const promiseChain=clients.openWindow(pageToOpen);
+    event.waitUntil(promiseChain);
+  });
+  
